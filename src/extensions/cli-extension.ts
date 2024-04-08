8a897f8ca3feaba5e5789524e1e7bcd83a31b54b
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-08 12:43:21 +0545

    feat: add ffmpeg command

--- src/extensions/cli-extension.ts
+++ src/extensions/cli-extension.ts
@@ -2,15 +2,11 @@ import { ExtendedGluegunToolbox, KnownError } from '../types'
 
 // add your CLI-specific functionality here, which will then be accessible to your commands
 module.exports = (toolbox: ExtendedGluegunToolbox) => {
-  toolbox.foo = () => {
-    toolbox.print.info('called foo extension')
-  }
   const ffmpegCli = toolbox.system.which('ffmpeg')
   if (!ffmpegCli) {
     throw new KnownError('FFMPEG not found')
   }
-  const ffmpeg = require('fluent-ffmpeg')
-  toolbox.ffmpeg = ffmpeg
+  toolbox.ffmpeg = require('fluent-ffmpeg')
 
   // enable this if you want to read configuration in from
   // the current folder's package.json (in a "meio" property),
