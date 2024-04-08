b18370b0fa4fc05fea54fed9eb492f9c9a3876cb
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-08 14:39:12 +0545

    feat: add types for app logic

--- src/commands/video/boomerang.ts
+++ src/commands/video/boomerang.ts
@@ -1,23 +1,18 @@
 import { GluegunCommand } from 'gluegun'
-import { ExtendedGluegunToolbox, KnownError } from '../../types'
+import { type ExtendedGluegunToolbox } from '../../types'
 import { breakVideoToSegmentsCommand } from '../../libs/video-utils'
 
 const command: GluegunCommand<ExtendedGluegunToolbox> = {
   name: 'boomerang',
   alias: ['boom'],
   run: async (toolbox) => {
-    const { ffmpeg, parameters, filesystem } = toolbox
+    const { media, parameters, filesystem } = toolbox
     const videoPath = parameters.first
-    if (!videoPath)
-      throw new KnownError('Use a video file as a first param to run')
-    const noFile = filesystem.isNotFile(videoPath)
-    if (noFile) throw new KnownError('Not a video file')
-    const stat = filesystem.inspect(videoPath, { absolutePath: true })
-    const vid = ffmpeg(videoPath)
-    const outputPath = filesystem.dir('output')
-    breakVideoToSegmentsCommand(vid, 30, outputPath).run()
-
-    console.log(stat, outputPath, filesystem.cwd())
+    const ffmpegCMD = media.video(videoPath)
+    console.log('file size', ffmpegCMD.videoSize, 'mb')
+    const outputPath = filesystem.dir('outpust')
+    const cmd = ffmpegCMD.makeChunks(30).outputPath(outputPath)
+    cmd.run()
 
     // ffmpeg -pattern_type glob -i "*.jpg" -filter_complex "[0]reverse[r];[0][r]concat,loop=5:250,setpts=N/25/TB,scale=1920:1080" -vcodec mpeg4 -q:v 1 output_looped.mp4
     // const { raw, argv, ...args } = parameters
--- src/libs/media.ts
+++ src/libs/media.ts
@@ -1,23 +1,48 @@
 import * as ffmpeg from 'fluent-ffmpeg'
+import * as jetpack from 'fs-jetpack'
+import { KnownError } from '../types'
+import { FSJetpack, InspectResult } from 'fs-jetpack/types'
 
 export class Video {
   private readonly videoPath: string
-  private readonly cmd: ffmpeg.FfmpegCommand
-  constructor(videoPath: string) {
-    this.videoPath = videoPath
-    this.cmd = ffmpeg(this.videoPath)
+  public readonly videoSize: number
+  private readonly videoFolder: string
+  public readonly cmd: ffmpeg.FfmpegCommand
+  constructor(video: InspectResult) {
+    this.videoFolder = jetpack.path(video.absolutePath, '..')
+    this.videoSize = +(video.size / 1024 ** 2).toFixed(2)
+    this.cmd = ffmpeg(video.absolutePath)
   }
-  public makeChunks(outputPath: string, segmentTimeInSec: number) {
-    return this.cmd
-      .outputOption('-map 0')
-      .outputOption('-c copy')
-      .outputOption('-f segment')
-      .outputOption(`-segment_time ${segmentTimeInSec}`)
-      .outputOption('-y')
-      .output(outputPath)
+
+  public makeChunks(segmentTimeInSec: number) {
+    const fileNamePrefix = 'video_'
+    return {
+      outputPath: (outputPath: FSJetpack, useCurrentDir = false) => {
+        const filePattern = `${fileNamePrefix}%04d.mp4`
+        const output = useCurrentDir
+          ? outputPath.path(filePattern)
+          : jetpack
+              .dir(jetpack.path(this.videoFolder, 'segments'))
+              .path(filePattern)
+        console.log(output)
+        return this.cmd
+          .outputOption('-map 0')
+          .outputOption('-c copy')
+          .outputOption('-f segment')
+          .outputOption(`-segment_time ${segmentTimeInSec}`)
+          .outputOption('-y')
+          .output(output)
+      },
+    }
   }
 }
 
 export function video(videoPath: string) {
-  return new Video(videoPath)
+  if (!videoPath)
+    throw new KnownError('Use a video file as a first param to run')
+  const noFile = jetpack.exists(videoPath)
+  if (noFile !== 'file') throw new KnownError('Not a video file')
+  const stat = jetpack.inspect(videoPath, { absolutePath: true })
+  if (stat.type !== 'file') throw new KnownError('Provided arg is not a file')
+  return new Video(stat)
 }
