bf3c646cec766cb3f7f772b32a7459d7faf0a550
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-08 14:52:51 +0545

    chore: remove unused functions

--- src/commands/video/boomerang.ts
+++ src/commands/video/boomerang.ts
@@ -5,15 +5,6 @@ const command: GluegunCommand<ExtendedGluegunToolbox> = {
   name: 'boomerang',
   alias: ['boom'],
   run: async (toolbox) => {
-    const { media, parameters, filesystem } = toolbox
-    const videoPath = parameters.first
-    const ffmpegCMD = media.video(videoPath)
-    console.log('file size', ffmpegCMD.videoSize, 'mb')
-    const outputPath = filesystem.dir('output')
-    const cmd = ffmpegCMD.makeChunks(30).outputPath(outputPath)
-    // console.log(cmd._getArguments())
-    cmd.run()
-
     // ffmpeg -pattern_type glob -i "*.jpg" -filter_complex "[0]reverse[r];[0][r]concat,loop=5:250,setpts=N/25/TB,scale=1920:1080" -vcodec mpeg4 -q:v 1 output_looped.mp4
     // const { raw, argv, ...args } = parameters
     // print.info('Welcome to your meio CLI')
