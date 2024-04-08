9205f926efdc26c5898dd402a22589bf6b9ee3af
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-08 16:24:22 +0545

    feat: add spy test

--- src/libs/video-utils.test.ts
+++ src/libs/video-utils.test.ts
@@ -1,5 +1,31 @@
+import { reverseVideoOrder } from './video-utils'
+import { expect, vi } from 'vitest'
+import jetpack = require('fs-jetpack')
+
 describe('video utils', () => {
   it('should reverse order of video files', () => {
-    expect(1).toBe(1)
+    const spy = vi.spyOn(jetpack, 'read')
+    spy.mockReturnValue(
+      'ffconcat version 1.0\n' +
+        '\n' +
+        'file video_0004.mp4\n' +
+        'file video_0003.mp4\n' +
+        'file video_0002.mp4\n' +
+        'file video_0001.mp4\n' +
+        'file video_0000.mp4\n',
+    )
+
+    const result = reverseVideoOrder('test.txt')
+    expect(spy).toBeCalled()
+    expect(result).toBe(
+      'ffconcat version 1.0\n' +
+        '\n' +
+        'file video_0000.mp4\n' +
+        'file video_0001.mp4\n' +
+        'file video_0002.mp4\n' +
+        'file video_0003.mp4\n' +
+        'file video_0004.mp4' +
+        '\n',
+    )
   })
 })
