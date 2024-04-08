177ee87cb694de61f7e6fc4ae951dc0d960f0f0f
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-08 12:42:35 +0545

    feat: add ffmpeg command

--- src/commands/meio.ts
+++ src/commands/meio.ts
@@ -1,12 +1,13 @@
 import { GluegunCommand } from 'gluegun'
+import { ExtendedGluegunToolbox } from '../types'
 
-const command: GluegunCommand = {
+const command: GluegunCommand<ExtendedGluegunToolbox> = {
   name: 'meio',
   run: async (toolbox) => {
     const { print, parameters } = toolbox
     const { raw, argv, ...args } = parameters
     print.info('Welcome to your meio CLI')
-    console.log(args)
+    console.log(args, toolbox.ffmpeg)
   },
 }
 
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,8 @@
-export class KnownError extends Error {
+import { GluegunToolbox } from 'gluegun'
+import * as Ffmpeg from 'fluent-ffmpeg'
 
+export class KnownError extends Error {}
+
+export interface ExtendedGluegunToolbox extends GluegunToolbox {
+  ffmpeg: Ffmpeg.FfmpegCommand
 }
