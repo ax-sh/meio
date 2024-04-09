79de97ec97640485a2cb1b1b285bc8a0b9d4e9cf
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-09 09:17:45 +0545

    feat: Update release-it configuration for better version handling

--- .release-it.cjs
+++ .release-it.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     "hooks": {
-        "before:init": "git fetch --prune --prune-tags origin",
+        "before:init": ["echo v${version}", "git fetch --prune --prune-tags origin", 'pnpm test',],
         "after:release": "echo Successfully released ${version} to ${repo.repository}."
     },
     "git": {
