3d5e567b774c14a5c05d0aa17b4e6f2a06f80f06
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-12 11:25:35 +0545

    fix: Fix parameter naming issue and add check for missing subtitle or video paths

--- src/commands/video/merge-subs.ts
+++ src/commands/video/merge-subs.ts
@@ -1,20 +1,26 @@
 import { GluegunCommand } from 'gluegun'
 import { type ExtendedGluegunToolbox, KnownError } from '../../types'
+import { dir } from 'fs-jetpack'
 
 const command: GluegunCommand<ExtendedGluegunToolbox> = {
   name: 'merge-subs',
   alias: ['merge-subs'],
   run: async (toolbox) => {
     const { filesystem, parameters } = toolbox
-    const dir = parameters.first
-    if (filesystem.isNotDirectory(dir))
+    const folder = parameters.first
+    if (!folder) throw new KnownError('Only dir supported')
+    if (filesystem.isNotDirectory(folder))
       throw new KnownError('Only dir supported')
+    const dir = filesystem.path(folder)
     const list = filesystem.list(dir)
+    console.log(dir)
+
     const mergeSubs = await import('../../libs/merge-subs.utils')
     const candidates = mergeSubs.findCandidates(list)
 
     const items = Object.entries(candidates)
     for await (const [_, item] of items) {
+      if (!item.subtitle || !item.video) continue
       const subtitlePath = filesystem.path(item.subtitle)
       const videoPath = filesystem.path(item.video)
       const outputPath = filesystem.dir('merged__output').path(item.outputPath)
