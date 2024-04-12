7cff84c10e37c671de2c36d0c9fd3b4e914216c6
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-04-12 11:04:10 +0545

    test: Add tests for finding and ignoring merged subtitle candidates

--- src/libs/merge-subs.utils.test.ts
+++ src/libs/merge-subs.utils.test.ts
@@ -1,13 +1,32 @@
-import { filesystem } from 'gluegun'
-
 describe('mergeSubs.utils.test', () => {
   it('should get candidates', async () => {
     const mergeSubs = await import('./merge-subs.utils')
-    const list = filesystem.list(
-      'C:/Users/USER/Desktop/video-with-external-subs',
-    )
+    const list = ['video.srt', 'video.mp4']
     const candidates = mergeSubs.findCandidates(list)
     expect(candidates).toBeDefined()
-    console.log(candidates, 77)
+    const expectedShape = {
+      video: {
+        video: expect.any(String),
+        subtitle: expect.any(String),
+        outputPath: expect.any(String),
+      },
+    }
+    expect(candidates).toMatchObject(expectedShape)
+  })
+  it('should ignore merged candidates', async () => {
+    const mergeSubs = await import('./merge-subs.utils')
+    const list = ['video.srt', 'video.mp4', 'video.merged-subs.mp4']
+    const candidates = mergeSubs.findCandidates(list)
+    expect(candidates).toBeDefined()
+    const expectedShape = {
+      video: {
+        video: expect.any(String),
+        subtitle: expect.any(String),
+        outputPath: expect.any(String),
+      },
+    }
+    console.log(candidates)
+    expect(candidates).toMatchObject(expectedShape)
+    expect(candidates).toEqual(expectedShape)
   })
 })
