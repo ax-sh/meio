80660c80671009bff14e9e6095b34499e60e4166
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-05-07 13:22:44 +0545

    test: Update fileSystem test for jetpack mock functionality

--- src/libs/fileSystem.test.ts
+++ src/libs/fileSystem.test.ts
@@ -1,15 +1,25 @@
 import jetpack = require('fs-jetpack')
+import { vi } from 'vitest'
 
-vi.mock('fs-jetpack', async () => {
-  const mod = await vi.importActual<typeof import('fs-jetpack')>('fs-jetpack')
-  return {
-    ...mod,
-    exists: vi.fn(() => true), // Create a mock function for exists that returns true
-  }
-})
+// vi.mock('fs-jetpack', async () => {
+//   const mod = await vi.importActual<typeof import('fs-jetpack')>('fs-jetpack')
+//   return {
+//     ...mod,
+//     exists: vi.fn(() => 'roooo'), // Create a mock function for exists that returns true
+//   }
+// })
 
 describe('fileSystem', () => {
   it('should test jetpack', () => {
-    console.log(jetpack.exists('/foo')) // This will log true
+    expect(jetpack.exists('/foo')).toBe(true)
+  })
+
+  it('should test jetpack mock', async () => {
+    const jetpackMock = await vi.importMock<{
+      default: typeof import('fs-jetpack')
+    }>('fs-jetpack')
+    jetpackMock.default.exists.mockReturnValue('moo')
+
+    expect(jetpackMock.default.exists('/foo')).toBe('moo')
   })
 })
