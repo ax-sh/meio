72b0124ad68f160e014327937894fc301eefca0e
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-05-11 16:10:27 +0545

    feat: add rmbg command to remove image background

---
+++ src/commands/rmbg.ts
@@ -0,0 +1,26 @@
+import type { GluegunCommand } from 'gluegun'
+
+const command: GluegunCommand = {
+  name: 'rmbg',
+  run: async (toolbox) => {
+    const { print, system, parameters } = toolbox
+    const { parseFilePath } = await import('../lib/parse-file-path')
+    const inputFile = parseFilePath(parameters.first)
+    const outputFile = inputFile.newFilePath('removed-bg')
+
+    try {
+      const output = await system.run(
+        `rembg i -m u2net_human_seg ${inputFile.path} ${outputFile}`,
+      )
+      print.info(output)
+      print.info(`rembg Done: ${inputFile.path}\n`)
+      print.success(outputFile)
+    } catch (e) {
+      print.error(e)
+      print.info(inputFile)
+      print.info(`${inputFile.fileExt}, 'rembg: Check extention'`)
+    }
+  },
+}
+
+module.exports = command
