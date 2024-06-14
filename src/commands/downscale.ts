0ac0f75dc577947db1a42fe393bc2eb91d56f66f
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-14 16:23:27 +0545

    feat: wip

--- src/commands/downscale.ts
+++ src/commands/downscale.ts
@@ -8,12 +8,22 @@ const command: GluegunCommand<ExtendedGluegunToolbox> = {
     const { print, parameters, filesystem, system } = toolbox
     const input = parameters.first
     const { makeOutputPath } = await import('../libs/make-output-path')
-    const { outputPath } = makeOutputPath(input)
+    const { outputPath, inputPath } = makeOutputPath(input)
+    const timer = toolbox.system.startTimer()
+    const cmd = `ffmpeg -hwaccel cuda -i "${inputPath}" -c:v libx265 -preset fast "${outputPath}"`
+    print.success(cmd)
 
-    // const out = await system.run(
-    //   `ffmpeg -hwaccel cuda -i ${inputPath} -c:v libx265 -preset fast ${outputPath}`,
-    // )
-    console.log(outputPath)
+    const spinner = print.spin('making h265 encode using cuda')
+
+    const out = await system.exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log(stdout)
+    })
+    spinner.succeed(out)
+    console.log(`that just took ${timer()} ms.`)
   },
 }
 
